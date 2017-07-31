6e69af596d4daf6b545406fa240cfc666c872020
rob smith <rmsmith@outlook.com>
2017-07-31 15:19:53 +0100

    use 2.7 and remove mpl & weights

--- kDS.sh
+++ kDS.sh
@@ -6,12 +6,13 @@
 
 
 # Activate python
-source /share/apps/examples/python-3.6.source.sh
+#source /share/apps/examples/python-3.6.source.sh
+source /home/rsmith/python-2.7.13.source.sh
 
 #export CUDA_VISIBLE_DEVICES=`nvidia-smi --query-gpu=index,memory.used --format=csv,nounits,noheader | awk -F "," '/, 0/ {print $1;exit}'` 
 
 #export CUDA_VISIBLE_DEVICES='1,2,-1,0'
-export CUDA_VISIBLE_DEVICES='0'
+export CUDA_VISIBLE_DEVICES='2'
 
 echo $CUDA_VISIBLE_DEVICES
 
@@ -21,12 +22,12 @@ LD_LIBRARY_PATH="/share/apps/libc6_2.17/lib/x86_64-linux-gnu/:/share/apps/libc6_
 
 # Lets run a tester GPU script
 echo 'test script test-tf'
-/share/apps/libc6_2.17/lib/x86_64-linux-gnu/ld-2.17.so $(command -v /share/apps/python-3.6.0-shared/bin/python3) /cluster/project2/darkspeech/rob/DeepSpeech/test-tf.py
+/share/apps/libc6_2.17/lib/x86_64-linux-gnu/ld-2.17.so $(command -v /home/rsmith/Python-2.7.13/bin/python) /cluster/project2/darkspeech/rob/DeepSpeech/test-tf.py
 
 
 # GPU speed test script
 #echo 'test script tf-GPU-test'
-/share/apps/libc6_2.17/lib/x86_64-linux-gnu/ld-2.17.so $(command -v /share/apps/python-3.6.0-shared/bin/python3) /cluster/project2/darkspeech/rob/DeepSpeech/gpu-test-tf.py
+/share/apps/libc6_2.17/lib/x86_64-linux-gnu/ld-2.17.so $(command -v /home/rsmith/Python-2.7.13/bin/python) /cluster/project2/darkspeech/rob/DeepSpeech/gpu-test-tf.py
 
 
 # Start main program
@@ -35,7 +36,7 @@ echo 'starting kDS script'
 SECONDS=0
 
 #kDS
-/share/apps/libc6_2.17/lib/x86_64-linux-gnu/ld-2.17.so $(command -v /share/apps/python-3.6.0-shared/bin/python3) /cluster/project2/darkspeech/rob/kDS/run-train.py 
+/share/apps/libc6_2.17/lib/x86_64-linux-gnu/ld-2.17.so $(command -v /home/rsmith/Python-2.7.13/bin/python) /cluster/project2/darkspeech/rob/kDS/run-train.py 
 
 
 duration=$SECONDS
--- run-train.py
+++ run-train.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 #from IPython.core.display import HTML, display
 #from IPython.display import display, Audio
